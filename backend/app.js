7c22ec3bb0317aebaa094d5282b408326dabd81e
emresates <seyf338@gmail.com>
2023-01-07 17:15:42 +0300

    auth route

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,19 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+const authRoute = require('./routes/auth');
 dotenv.config();
+
 const app = express();
+app.use(express.json());
 
 mongoose.set('strictQuery', false);
 mongoose
   .connect(process.env.MONGO_URL_TEMP)
   .then(console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
+
+app.use('/backend/auth', authRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
