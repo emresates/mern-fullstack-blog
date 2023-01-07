a65afa50cc0bab63ac58e0c19dc7ad5a95c6237d
emresates <seyf338@gmail.com>
2023-01-07 19:03:43 +0300

    categories and photo added

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
+const categoriesRoute = require('./routes/categories');
 
 dotenv.config();
 
@@ -17,9 +19,23 @@ mongoose
   .then(console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, 'hello.jpeg');
+  },
+});
+const upload = multer({ storage: storage });
+app.post('/backend/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
 app.use('/backend/auth', authRoute);
 app.use('/backend/users', userRoute);
 app.use('/backend/posts', postRoute);
+app.use('/backend/categories', categoriesRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
