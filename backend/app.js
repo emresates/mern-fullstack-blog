4e14267e3781c5ad26dcd3d8dc039eb2fadf1f56
emresates <seyf338@gmail.com>
2023-01-07 16:05:00 +0300

    backend conf

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+dotenv.config();
 const app = express();
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_URL_TEMP)
+  .then(console.log('Connected to MongoDB'))
+  .catch((err) => console.log(err));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log('Backend is running');
+  console.log(`Backend is running on port ${port}`);
 });
