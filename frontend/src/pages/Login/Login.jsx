12bf842e3e8c5ada6faf00f95ccdeb56483dc55d
emresates <seyf338@gmail.com>
2023-01-08 18:42:35 +0300

    creating context api login process steps 1

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useContext, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Context } from '../../context/Context';
 
 function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+
+  const { dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: 'LOGIN_START' });
+    try {
+      const res = await axios.post('/auth/login', {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: 'LOGIN_SUCCESS', payload: res.data });
+    } catch (err) {
+      dispatch({ type: 'LOGIN_FAILURE' });
+    }
+  };
+
   return (
     <div className="login">
       <span>Login</span>
-      <form action="">
-        <label htmlFor="">Email</label>
-        <input type="text" placeholder="Your e-mail" />
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor="">Username</label>
+        <input type="text" placeholder="Your username..." ref={userRef} />
         <label htmlFor="">Password</label>
-        <input type="password" placeholder="Your password" />
-        <button type="submit">Login</button>
+        <input type="password" placeholder="Your password" ref={passwordRef} />
+        <button type="submit" disabled={isFetching}>
+          Login
+        </button>
       </form>
       <h3>
         Don't you have an account. Visit{' '}
